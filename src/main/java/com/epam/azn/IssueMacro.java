3b8a94a76e3d2c1ae36cdee4c2aeb47eecd5ccb7
Roman Dmitriev <dmitriev.rs@gmail.com>
2016-12-20 17:24:45 +0300

    pure skeleton commit

--- src/main/java/com/epam/azn/IssueMacro.java
+++ src/main/java/com/epam/azn/IssueMacro.java
@@ -4,23 +4,32 @@ import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+import com.atlassian.applinks.api.ApplicationLinkService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 
 @Scanned
 public class IssueMacro implements Macro{
 
+    private final ApplicationLinkService applicationLinkService;
 
+    @Autowired
+    public IssueMacro(@ComponentImport final ApplicationLinkService applicationLinkService) {
+        this.applicationLinkService = applicationLinkService;
+    }
 
     public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
         return null;
     }
 
     public BodyType getBodyType() {
-        return null;
+        return BodyType.NONE;
     }
 
     public OutputType getOutputType() {
-        return null;
+        return OutputType.BLOCK;
     }
 }
